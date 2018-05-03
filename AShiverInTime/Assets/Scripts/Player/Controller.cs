5ee3c78ca096288ab73e84a5b4062f042bdcad47
Anne Wijning <amwijning@hotmail.com>
2018-05-03 22:21:48 +0200

    Incremental update

--- AShiverInTime/Assets/Scripts/Player/Controller.cs
+++ AShiverInTime/Assets/Scripts/Player/Controller.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider))]
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Player_SpriteController))]
 public class Controller : MonoBehaviour {
 
+	static public Controller instance;
     BoxCollider playerCollider;
     Rigidbody playerRigidBody;
+	private Player_SpriteController spriteController;
 
     public float verticalDrag = 1f;
     public float baseSpeed = 1f;
@@ -15,28 +18,58 @@ public class Controller : MonoBehaviour {
     public float drag = 1f;
     public float runModifier = 2f;
     Vector3 movement;
+
+	public float jumpingAnimationTime = 1f/30f;
+	private float jumpingTimer;
+
+	public float fallingSpeedTreshold = -0.1f;
+	public float duckTimer = 0.0f;
+
+	public GameObject playerCorpsePrefab;
+
+	static public float levelTimer;
     
     // Use this for initialization
     private void Awake () {
         playerCollider = GetComponent<BoxCollider>();
         playerRigidBody = GetComponent<Rigidbody>();
+		spriteController = GetComponent<Player_SpriteController> ();
+		instance = this;
+
+		ResetTimer ();
 	}
-	
+
+	public void Die(){
+
+		if(levelTimer < 0.0f)
+			levelTimer = 0.0f;
+
+		gameObject.SetActive (false);
+		GameObject temp = Instantiate (playerCorpsePrefab, transform.position, transform.rotation) as GameObject;
+		temp.GetComponent<Player_Corpse> ().InitVelocity(movement);
+
+		Level_Manager.OnPlayerDied ();
+	}
+
 	// Update is called once per frame
 	private void Update () {
 
+		bool isGrounded = Mathf.Abs (playerRigidBody.velocity.y) < 0.05f;
 		Vector2 input = new Vector2 (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
 
-        if(Input.GetButtonDown("Fire2"))//a
+        if(Input.GetButtonDown("Fire2") && isGrounded)//a
         {
             Debug.Log("A");
             movement.y += jumpPower;
             playerRigidBody.velocity = Vector3.zero;
+			jumpingTimer = jumpingAnimationTime;
         }
 
         if (Input.GetButtonDown("Fire1"))//b
         {
             Debug.Log("B");
+			Die ();
+
         }
 
         if (Input.GetButtonDown("Start"))//start
@@ -78,18 +111,53 @@ public class Controller : MonoBehaviour {
         //slowdown and gravity
         if(movement.x > 0f)
         {
-            movement.x -= drag * Time.deltaTime;
+			movement.x = Mathf.Max(movement.x - (drag * Time.deltaTime), 0.0f);
         }
         else if(movement.x < 0f)
         {
-            movement.x += drag * Time.deltaTime;
+			movement.x = Mathf.Min(movement.x + (drag * Time.deltaTime), 0.0f);
         }
 		if(movement.y > 0f)
 		{
-			movement.y -= verticalDrag * Time.deltaTime;
+			movement.y = Mathf.Max(movement.y - (verticalDrag * Time.deltaTime), 0.0f);
 		}
 
         movement = new Vector3(Mathf.Clamp(movement.x, -baseSpeed, baseSpeed), Mathf.Clamp(movement.y, Physics.gravity.y, jumpPower),0f);
         playerRigidBody.position = transform.position + (movement * Time.deltaTime);
+
+		//Animation block
+		jumpingTimer -= Time.deltaTime;
+		if (jumpingTimer > 0.0f)
+			spriteController.SetAnimation (AnimationState.jumping);
+		else if (playerRigidBody.velocity.y < fallingSpeedTreshold)
+			spriteController.SetAnimation (AnimationState.falling);
+		else {
+			if (Mathf.Abs (movement.x) > 0.0f)
+				spriteController.SetAnimation (AnimationState.walking);
+			else if (input.y < -deadzone) {
+				spriteController.SetAnimation (AnimationState.ducking);
+				duckTimer += Time.deltaTime;
+			}
+			else
+				spriteController.SetAnimation (AnimationState.idle);
+		}
+
+		if (spriteController.GetAnimationState () != AnimationState.ducking)
+			duckTimer = 0.0f;
+
+		if (input.x > deadzone) 
+			spriteController.SetFacing (false);
+		else if (input.x < -deadzone) 
+			spriteController.SetFacing (true);
+			
+
+		levelTimer -= Time.deltaTime;
+		if (levelTimer < 0.0f || transform.position.y <= Level_Manager.instance.killY)
+			Die ();
+	}
+
+	static public void ResetTimer(){
+
+		levelTimer = Level_Manager.instance.levelTime;
 	}
 }
---
+++ AShiverInTime/Assets/Scripts/Player/Player_Corpse.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player_Corpse : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float flipTime;
+	public float flipSpeed;
+	public float verticalLaunchSpeed = 10.0f;
+
+	private void Awake () {
+
+		rb = gameObject.GetComponent<Rigidbody> ();
+
+		if(Random.value > 0.5)
+			flipSpeed = -flipSpeed;
+	}
+
+	public void InitVelocity(Vector3 inheritedVelocity){
+
+		rb.velocity = new Vector3 (inheritedVelocity.x, verticalLaunchSpeed, 0.0f);
+	}
+
+	private void OnCollisionEnter(Collision other){
+
+		Destroy (this);
+	}
+
+	private void Update () {
+
+		flipTime -= Time.deltaTime;
+
+		Vector3 angles = transform.eulerAngles;
+		rb.rotation = Quaternion.Euler (angles.x, angles.y, angles.z + (flipSpeed * Time.deltaTime));
+
+		if (flipTime < 0.0f)
+			Destroy (this);
+	}
+}
