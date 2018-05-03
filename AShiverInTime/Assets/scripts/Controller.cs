74c5acd8ce656f66ca1f11119a8bd18daab82bd5
Toonyoshi123 <lisasijnstra@live.nl>
2018-05-03 14:52:40 +0200

    uploaded starting files

---
+++ AShiverInTime/Assets/scripts/Controller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class Controller : MonoBehaviour {
+
+    BoxCollider2D playerCollider;
+    Rigidbody2D playerRigidBody;
+
+
+	// Use this for initialization
+	void Awake () {
+        playerCollider = GetComponent<BoxCollider2D>();
+        playerRigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
